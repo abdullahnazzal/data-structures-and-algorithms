0dfc1b8457c79336c8a89d7687297116cca41ed0
abdullahnazzal <a.nazzal1995@gmail.com>
2021-10-14 18:13:04 +0300

    feat: add Node,linkedlist classes,with these functions

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,90 @@
+from typing import Any
+
+
+class Node:
+    """
+    class Node to represent the node
+
+    properties:
+    data = value stored in the Node
+    next_node = a pointer to the next Node
+    """
+    def __init__(self,value,next_node=None):
+        self.value=value
+        self.next_node=next_node
+
 class LinkedList:
     """
-    Put docstring here
-    """
+    A class for creating instances of a Linked List.
+  
+    Attributes defined here:
+    
+    head: Node | None
+    current: head | head
 
+    Methods defined here
+
+    insert(value: any)
+    contains(value: any) -> bool
+
+    """
     def __init__(self):
         # initialization here
-        pass
+        self.head=None
+        self.current=self.head
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self,value):
+        """
+        Adds a new node with that value to the head of the list with an O(1) Time performance.
+
+        Arguments: value
+        Returns: nothing
+        """
+        self.head=Node(value,self.head)
+        self.current=self.head
+    
+
+    def includes(self,value=None):
+        """
+        Indicates whether that value exists as a Node’s value somewhere within the list.
+
+        Arguments: value
+        Returns: Boolean
+        """
+        while self.current != None:
+            if self.current.value==value:
+                return True
+            self.current=self.current.next_node
+        return False
+    
+    def to_string(self):
+        """
+        To convert Linked List as string formatted
+        Arguments: none
+        Returns: a string representing all the values in the Linked List, formatted as:
+        "{ a } -> { b } -> { c } -> NULL"
+        """
+        string_list=""
+        self.current=self.head
+        while self.current != None:
+            string_list+="{ "+"{}".format(self.current.value)+" } -> "
+            self.current=self.current.next_node
+            if self.current == None:
+                string_list+="NULL"
+        
+        return string_list
+
+# linked_list=LinkedList()
+# linked_list.insert(6)
+# linked_list.includes(5)
+# print(linked_list.includes(6))
+# print(linked_list.head.data)
+# linked_list.insert(7)
+# linked_list.insert(9)
+# print(linked_list.head.data)
+# linked_list.includes()
+# print(linked_list.includes(6))
+# linked_list.print_listedlink()
+# linked_list.to_string()
+
+# print(linked_list.to_string())
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,119 @@
-from linked_list.linked_list import LinkedList
-
+from typing import Any
+from linked_list.linked_list import LinkedList,Node
+import pytest
 
 def test_import():
     assert LinkedList
+
+def test_node_has_integer_value():
+    #Arrange
+    node=Node(1)
+    expected = 1
+
+    #Act
+    actual =node.value
+    #Assert
+    assert actual== expected
+
+def test_node_has_string_value():
+    #Arrange
+    node=Node("a")
+    expected = "a"
+
+    #Act
+    actual =node.value
+    #Assert
+    assert actual== expected
+
+def test_node_is_a_Node():
+    #Arrange
+    node=Node("")
+    expected = "Node"
+
+    #Act
+    actual =type(node).__name__
+    #Assert
+    assert actual== expected
+
+def test_node_witout_value():
+    with pytest.raises(TypeError):
+        node = Node()
+
+def test_new_linked_List_is_empty():
+    #Arrange
+    linked_list=LinkedList()
+    expected = None
+
+    #Act
+    actual =linked_list.head
+    #Assert
+    assert actual== expected
+
+def test_linked_List_insert():
+    #Arrange
+    linked_list=LinkedList()
+    linked_list.insert(6)
+    expected = 6
+
+    #Act
+    actual =linked_list.head.value
+    #Assert
+    assert actual== expected
+
+def test_linked_List_insert_twice():
+    # Arrange
+    expected = 0
+    linked_list = LinkedList()
+
+    # Act
+    linked_list.insert(1)
+    linked_list.insert(0)
+    node = linked_list.head
+    actual = node.value
+
+    # Assert
+    assert actual == expected
+    assert linked_list.head.next_node.value == 1
+
+def test_a_value_includes_in_linked_List():
+    # Arrange
+    linked_list = LinkedList()
+    expected = True
+
+    # Act
+    linked_list.insert(1)
+    linked_list.insert(2)
+    linked_list.insert(3)
+    actual = linked_list.includes(2)
+
+    # Assert
+    assert actual == expected
+
+def test_a_value_not_includes_in_linked_List():
+    # Arrange
+    linked_list = LinkedList()
+    expected = False
+
+    # Act
+    linked_list.insert(1)
+    linked_list.insert(2)
+    linked_list.insert(3)
+    actual = linked_list.includes(5)
+
+    # Assert
+    assert actual == expected
+
+def test_linked_List_to_string():
+    # Arrange
+    linked_list = LinkedList()
+    expected = '{ 3 } -> { 2 } -> { 1 } -> NULL'
+                
+
+    # Act
+    linked_list.insert(1)
+    linked_list.insert(2)
+    linked_list.insert(3)
+    actual = linked_list.to_string()
+
+    # Assert
+    assert actual == expected
